aa7f486beba74df72b1d1ba97a0c469cc48701e9
Josef Kuba <567791@mail.muni.cz>
2025-06-04 17:43:23 +0200

    feat: DataInitiallizer adding courts

--- src/main/java/com/inqool/tennisclub/config/DataInitializer.java
+++ src/main/java/com/inqool/tennisclub/config/DataInitializer.java
@@ -1,6 +1,8 @@
 package com.inqool.tennisclub.config;
 
+import com.inqool.tennisclub.data.model.CourtEntity;
 import com.inqool.tennisclub.data.model.CourtSurfaceEntity;
+import com.inqool.tennisclub.data.repository.CourtRepository;
 import com.inqool.tennisclub.data.repository.CourtSurfaceRepository;
 import jakarta.transaction.Transactional;
 import java.math.BigDecimal;
@@ -18,9 +20,11 @@ import org.springframework.stereotype.Component;
 public class DataInitializer {
 
     private final CourtSurfaceRepository courtSurfaceRepository;
+    private final CourtRepository courtRepository;
 
-    public DataInitializer(CourtSurfaceRepository courtSurfaceRepository) {
+    public DataInitializer(CourtSurfaceRepository courtSurfaceRepository, CourtRepository courtRepository) {
         this.courtSurfaceRepository = courtSurfaceRepository;
+        this.courtRepository = courtRepository;
     }
 
     @EventListener(ApplicationReadyEvent.class)
@@ -28,23 +32,46 @@ public class DataInitializer {
     public void initializeData() {
         log.warn("Initializing CourtSurfaces!");
 
-        CourtSurfaceEntity entity1 = new CourtSurfaceEntity();
-        entity1.setSurfaceName("Grass");
-        entity1.setSurfaceDescription("Short grass 7mm");
-        entity1.setCostPerMinute(BigDecimal.valueOf(1.2));
+        CourtSurfaceEntity surfaceEntity1 = new CourtSurfaceEntity();
+        surfaceEntity1.setSurfaceName("Grass");
+        surfaceEntity1.setSurfaceDescription("Short grass 7mm");
+        surfaceEntity1.setCostPerMinute(BigDecimal.valueOf(1.2));
 
-        CourtSurfaceEntity entity2 = new CourtSurfaceEntity();
-        entity2.setSurfaceName("Hard");
-        entity2.setSurfaceDescription("Blue rubber");
-        entity2.setCostPerMinute(BigDecimal.valueOf(.1));
+        CourtSurfaceEntity surfaceEntity2 = new CourtSurfaceEntity();
+        surfaceEntity2.setSurfaceName("Hard");
+        surfaceEntity2.setSurfaceDescription("Blue rubber");
+        surfaceEntity2.setCostPerMinute(BigDecimal.valueOf(.1));
 
-        CourtSurfaceEntity entity3 = new CourtSurfaceEntity();
-        entity3.setSurfaceName("Clay");
-        entity3.setSurfaceDescription("Red clay");
-        entity3.setCostPerMinute(BigDecimal.valueOf(0.2));
+        CourtSurfaceEntity surfaceEntity3 = new CourtSurfaceEntity();
+        surfaceEntity3.setSurfaceName("Clay");
+        surfaceEntity3.setSurfaceDescription("Red clay");
+        surfaceEntity3.setCostPerMinute(BigDecimal.valueOf(0.2));
 
-        courtSurfaceRepository.save(entity1);
-        courtSurfaceRepository.save(entity2);
-        courtSurfaceRepository.save(entity3);
+        courtSurfaceRepository.save(surfaceEntity1);
+        courtSurfaceRepository.save(surfaceEntity2);
+        courtSurfaceRepository.save(surfaceEntity3);
+
+        log.warn("Initializing Courts!");
+
+        CourtEntity courtEntity1 = new CourtEntity();
+        courtEntity1.setCourtNumber(1);
+        courtEntity1.setCourtSurface(surfaceEntity2);
+
+        CourtEntity courtEntity2 = new CourtEntity();
+        courtEntity2.setCourtNumber(2);
+        courtEntity2.setCourtSurface(surfaceEntity2);
+
+        CourtEntity courtEntity3 = new CourtEntity();
+        courtEntity3.setCourtNumber(3);
+        courtEntity3.setCourtSurface(surfaceEntity3);
+
+        CourtEntity courtEntity4 = new CourtEntity();
+        courtEntity4.setCourtNumber(4);
+        courtEntity4.setCourtSurface(surfaceEntity1);
+
+        courtRepository.save(courtEntity1);
+        courtRepository.save(courtEntity2);
+        courtRepository.save(courtEntity3);
+        courtRepository.save(courtEntity4);
     }
 }
